26f396f318d5ccbbc6c6ab170215907c0479ffcf
sakuramoti0125 <stsu23hiro@icloud.com>
2024-12-06 15:49:45 +0900

    カレンダー

--- test.bash
+++ test.bash
@@ -19,25 +19,25 @@ Mo Tu We Th Fr Sa Su
 EOF
 )
 
-out=$(echo "2024 11" |  ./show_calendar)
+out=$(echo "2024 11" |  ./show_calendar 2>&1)
 if ! diff <(echo "$out" | sed '/^\s*$/d') <(echo "$expected_output" | sed '/^\s*$/d'); then
     ng "${LINENO}"
 fi
 
 # テストケース 2: 月が範囲外の場合
-out=$(echo "2024 13" | ./show_calendar)
+out=$(echo "2024 13" | ./show_calendar 2>&1)
 [ "${out}" = "月は1から12の間で入力してください。" ] || ng "${LINENO}"
 
 # テストケース 3: 無効な文字列入力
-out=$(echo "2024 November" | ./show_calendar)
+out=$(echo "2024 November" | ./show_calendar 2>&1)
 [ "${out}" = "無効な入力です。年と月は整数で、スペースで区切って入力してください。" ] || ng "${LINENO}"
 
 # テストケース 4: 入力が不足している場合
-out=$(echo "2024" | ./show_calendar)
+out=$(echo "2024" | ./show_calendar 2>&1)
 [ "${out}" = "無効な入力です。年と月は整数で、スペースで区切って入力してください。" ] || ng "${LINENO}"
 
 # テストケース 5: 空入力
-out=$(echo " " | show_calendar)
+out=$(echo " " | show_calendar 2>&1)
 [ "${out}" = "無効な入力です。年と月は整数で、スペースで区切って入力してください。" ] || ng "${LINENO}"
 
 
